4344dea66ce4df533e4fa4de3426bc59e2527d9b
mirjambilovj <106926296+mirjambilovj@users.noreply.github.com>
2022-10-20 05:11:02 +0500

    done

--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,21 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal{
+    public Bird() {
+        super();
+    }
 
+    Bird(String color, int numberOfPaws, boolean hasFur) {
+        super(color, numberOfPaws, hasFur);
+        color = "blue";
+        numberOfPaws = 2;
+        hasFur = false;
+    }
+
+    @Override
+    public String getDescription() {
+        String myStr;
+        myStr = "This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.";
+        return myStr;
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Dog.java
+++ src/main/java/com/epam/mjc/stage0/Dog.java
@@ -1,5 +1,21 @@
 package com.epam.mjc.stage0;
 
-public class Dog {
+public class Dog extends Animal{
+    public Dog() {
+        super();
+    }
 
+    Dog(String color, int numberOfPaws, boolean hasFur) {
+        super(color, numberOfPaws, hasFur);
+        color = "brown";
+        numberOfPaws = 4;
+        hasFur = true;
+    }
+
+    @Override
+    public String getDescription() {
+        String myStr;
+        myStr = "This animal is mostly brown. It has 4 paws and a fur.";
+        return myStr;
+    }
 }
